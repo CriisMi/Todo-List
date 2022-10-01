ebf403489119d79f70ff15a1cf9b559c0f6fd78b
Cris <cristinamihalciuc@yahoo.com>
2022-10-01 17:12:54 +0300

    Add new project button

--- src/dom-new-project-template.js
+++ src/dom-new-project-template.js
@@ -1,7 +1,17 @@
+import plusImg from './img/add.png';
+
 export default function projectTemplateNew() {
     let content = document.querySelector('.content');
 
     let projectTemplate = document.createElement('div');
     projectTemplate.setAttribute('class', 'projectSectionAdd');
+
+    let addButton = document.createElement('button');
+    addButton.setAttribute('class', 'add-new-project-btn');
+    let btnImg = document.createElement('img');
+    btnImg.src = plusImg;
+    addButton.appendChild(btnImg);
+    projectTemplate.appendChild(addButton);
+
     content.appendChild(projectTemplate);
 }
